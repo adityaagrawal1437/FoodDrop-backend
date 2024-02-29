e47c8453157006a3ff67f7619ba00fc1a94f8a0c
Aditya Agrawal <2021ugee043@nitjsr.ac.in>
2024-02-29 20:59:18 +0530

    corrected import of userroutes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import UserRoute from "../src/Routes/UserRoutes";
+import UserRoute from "./Routes/UserRoutes";
 mongoose.connect(process.env.DATABASE as string).then(() => {
   console.log("Connected to database!");
 });
@@ -10,9 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/health",async(req:Request,res:Response)=>{
-  res.send({message:"health OK!"})
-})
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
 
 app.use("/api/my/user", UserRoute);
 app.listen(7001, () => {
